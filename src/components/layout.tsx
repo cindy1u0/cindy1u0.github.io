0f14a17a46da3b27d7f8757fdecc7ceb56e29675
Cindy Luo <cindyluo@fb.com>
2022-12-28 16:57:48 -0500

    layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
+  location: string;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, location }) => {
+  return <>yes</>;
+};
+
+export default Layout;
