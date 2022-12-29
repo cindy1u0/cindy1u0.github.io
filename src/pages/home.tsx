e61649d4b9cbaf83d9dc92740d642d5e5ae67039
Cindy Luo <cindyluo2015@gmail.com>
2022-12-29 02:45:58 -0500

    simple about page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
+import TopScroll from '../components/topScroll';
 
 const Wrapper = styled.section`
+  box-sizing: border-box;
   height: 100vh;
+  padding: 18% 10%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TitleText = styled.span`
+  font-size: 24px;
+`;
+
+const NameText = styled.span`
+  font-size: 128px;
+  font-weight: 600;
+`;
+
+const PositionGroup = styled.div`
+  display: flex;
+  margin-top: 16px;
+`;
+
+const PositionContainer = styled.div`
+  border: 1px #000 solid;
+  border-radius: 50px;
+  font-size: 20px;
+  margin-right: 16px;
+  padding: 10px 40px;
 `;
 
 const Home = (): JSX.Element => {
-  return <Wrapper>Home</Wrapper>;
+  const positions: string[] = ['Developer', 'Photographer', 'Musician'];
+  return (
+    <Wrapper>
+      <TopScroll />
+      <Container>
+        <TitleText>Hello! My name is</TitleText>
+        <NameText>Cindy Luo</NameText>
+        <PositionGroup>
+          {positions.map((pos: string, idx: number) => {
+            return <PositionContainer key={idx}>{pos}</PositionContainer>;
+          })}
+        </PositionGroup>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Home;
