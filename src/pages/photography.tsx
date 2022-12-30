bf721eb87226b25fc449dfb7401334fbbc655fd6
Cindy Luo <cindyluo2015@gmail.com>
2022-12-29 20:27:51 -0500

    photography done (#5)

--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
+import Headshot from '../images/me.jpeg';
 import styled from 'styled-components';
+import Gallery from 'react-photo-gallery';
+import { PHOTOS } from '../utils/photoList';
 
 const Wrapper = styled.section`
-  height: 100vh;
+  margin-bottom: 10%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 12%;
+`;
+
+const TitleText = styled.h1`
+  font-size: 84px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  margin-top: 120px;
+  position: absolute;
+`;
+
+const NumberText = styled.span`
+  font-size: 16px;
+  right: 0;
+  position: absolute;
+  top: -25px;
+`;
+
+const HorizontalLine = styled.span`
+  border-top: 1px #000 solid;
+  width: 200px;
 `;
 
 const Photography = (): JSX.Element => {
-  return <Wrapper>Photography</Wrapper>;
+  return (
+    <Wrapper>
+      <TitleContainer>
+        <NumberText>05.</NumberText>
+        <HorizontalLine />
+      </TitleContainer>
+      <Container>
+        <TitleText>Photography</TitleText>
+        <Gallery photos={PHOTOS} />
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Photography;
---
+++ src/utils/photoList.ts
@@ -0,0 +1,247 @@
+import Image1 from '../images/photos/1.jpg';
+import Image2 from '../images/photos/2.jpg';
+import Image3 from '../images/photos/3.jpg';
+import Image4 from '../images/photos/4.jpg';
+import Image5 from '../images/photos/5.jpg';
+import Image6 from '../images/photos/6.jpg';
+import Image7 from '../images/photos/7.jpg';
+import Image8 from '../images/photos/8.jpg';
+import Image9 from '../images/photos/9.jpg';
+import Image10 from '../images/photos/10.jpg';
+import Image11 from '../images/photos/11.jpg';
+import Image12 from '../images/photos/12.jpg';
+import Image13 from '../images/photos/13.jpg';
+import Image14 from '../images/photos/14.jpg';
+import Image15 from '../images/photos/15.jpg';
+import Image16 from '../images/photos/16.jpg';
+import Image17 from '../images/photos/17.jpg';
+import Image18 from '../images/photos/18.jpg';
+import Image19 from '../images/photos/19.jpg';
+import Image20 from '../images/photos/20.jpg';
+import Image21 from '../images/photos/21.jpg';
+import Image22 from '../images/photos/22.jpg';
+import Image23 from '../images/photos/23.jpg';
+import Image24 from '../images/photos/24.jpg';
+import Image25 from '../images/photos/25.jpg';
+import Image26 from '../images/photos/26.jpg';
+import Image27 from '../images/photos/27.jpg';
+import Image28 from '../images/photos/28.jpg';
+import Image29 from '../images/photos/29.jpg';
+import Image30 from '../images/photos/30.jpg';
+import Image31 from '../images/photos/31.jpg';
+import Image32 from '../images/photos/32.jpg';
+import Image33 from '../images/photos/33.png';
+import Image34 from '../images/photos/34.png';
+import Image35 from '../images/photos/35.png';
+import Image36 from '../images/photos/36.png';
+import Image37 from '../images/photos/37.png';
+import Image38 from '../images/photos/38.png';
+import Image39 from '../images/photos/39.jpg';
+import Image40 from '../images/photos/40.jpg';
+
+export const PHOTOS: Array<{
+  src: string;
+  width: number;
+  height: number;
+}> = [
+  {
+    src: Image10,
+    width: 3,
+    height: 2,
+  },
+  {
+    src: Image12,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image39,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image40,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image6,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image2,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image8,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image9,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: Image3,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: Image1,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image7,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image14,
+    width: 3,
+    height: 3,
+  },
+  {
+    src: Image4,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image11,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image13,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image19,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image5,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: Image17,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image15,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image16,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image18,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image20,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image21,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image22,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image23,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image24,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image25,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image26,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: Image27,
+    width: 5,
+    height: 4,
+  },
+  {
+    src: Image28,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: Image29,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: Image30,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image31,
+    width: 4,
+    height: 4,
+  },
+  {
+    src: Image32,
+    width: 4,
+    height: 3,
+  },
+  {
+    src: Image33,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image34,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: Image35,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image36,
+    width: 5,
+    height: 3,
+  },
+  {
+    src: Image37,
+    width: 3,
+    height: 4,
+  },
+  {
+    src: Image38,
+    width: 4,
+    height: 4,
+  },
+];
